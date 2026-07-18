99a765bfad60e7c7370406564356b0b2a2694dc7
Elyashiv <elyashiv988@gmail.com>
2026-07-18 22:39:41 +0300

    added 5

--- main.py
+++ main.py
@@ -38,3 +38,28 @@ car = CarDelivery()
 
 print(drone.deliver(202))
 print(car.deliver(202))
+
+# 3 
+
+class DeliveryMethod(ABC):
+    def __init__(self,company_name):
+        self.company_name=company_name
+    @abstractmethod
+    def deliver(self,order_id):
+            pass
+class BikeDelivery(DeliveryMethod):
+    def __init__(self, company_name):
+        super().__init__(company_name)
+    def deliver(self, order_id):
+        return f"[{self.company_name}] Order {order_id} - bike_delivery"
+
+class DroneDelivery(DeliveryMethod):
+    def __init__(self, company_name):
+        super().__init__(company_name)
+    def deliver(self, order_id):
+        return f"[{self.company_name}] Order {order_id} - bike_delivery"
+    
+bike=BikeDelivery("SpeedRiders")
+print(bike.deliver(303))
+drone=DroneDelivery("SkyEx")
+print(drone.deliver(303))
